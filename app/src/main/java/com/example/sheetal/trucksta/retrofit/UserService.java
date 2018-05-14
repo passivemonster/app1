5a496eb92bdf5ec74d84d480a3dd47b8660e51f1
suryamurugan <surya3542live@gmail.com>
2018-05-14 10:18:13 +0530

    Retrofit interface needs to be setup!

--- app/src/main/java/com/example/sheetal/trucksta/retrofit/UserService.java
+++ app/src/main/java/com/example/sheetal/trucksta/retrofit/UserService.java
@@ -1,7 +1,38 @@
 package com.example.sheetal.trucksta.retrofit;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface UserService {
+    /*
+    @GET("vendorm.php")
+    Call<ArrayList<MediaObject>> vendorm(
+            @Query("id") String id);
 
 
-
+    @FormUrlEncoded
+    @POST("mediaupload.php")
+    Call<Void> mediaUpload(@Field("vendorID")String vendorID,
+                           @Field("mediaType")String mediaType,
+                           @Field("landmark")String landmark,
+                           @Field("facing")String facing,
+                           @Field("width")String width,
+                           @Field("height")String height,
+                           @Field("street")String street,
+                           @Field("city")String city,
+                           @Field("state")String state,
+                           @Field("available") String available,
+                           @Field("price")String price,
+                           @Field("price6")String price6,
+                           @Field("price12")String price12,
+                           @Field("imagedata")String imagedata,
+                           @Field("longitude")String longitude,
+                           @Field("latitude")String latitude);
+*/
 }
